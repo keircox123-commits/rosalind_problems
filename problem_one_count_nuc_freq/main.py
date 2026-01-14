aeb79de7290a557770b1607e684fcadddad4757a
Keir Cox <keircox123@gmail.com>
2026-01-14 15:31:32 +1100

    completed rosalind problem 1

---
+++ problem_one_count_nuc_freq/main.py
@@ -0,0 +1,17 @@
+import sys
+import os 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from DNA_functions import *
+from structures import *
+
+def main():
+    with open('/Users/keircox/Downloads/rosalind_dna.txt','r') as file:
+        seq = file.read()
+    seq = seq.upper()
+
+    print(*count_nucleotides(seq).values())
+
+
+if __name__ == '__main__':
+    main()

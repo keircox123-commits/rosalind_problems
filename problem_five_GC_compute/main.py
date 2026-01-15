7f03166d3ef0df49126911d4fefa9f8931c92729
Keir Cox <keircox123@gmail.com>
2026-01-16 10:06:16 +1100

    added gc_content function to functions

--- problem_five_GC_compute/main.py
+++ problem_five_GC_compute/main.py
@@ -5,12 +5,19 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Rosalind_functions import *
 from structures import *
 
+def main():
 
+    path = '/Users/keircox/Downloads/rosalind_gc-2.txt'
 
-path = '/Users/keircox/Downloads/rosalind_gc.txt'
+    genes = fasta_reader(path)
+    gc = {}
 
-genes = fasta_reader(path)
+    for key,value in genes.items():
+        gc[key] = value.count('G') + value.count('C')
 
+    max_key = max(gc,key=gc.get)
 
-for key, value in genes.items():
-    print(f'name: {key} seq: {value}')
+    print(f'{max_key} {gc[max_key]}')
+
+if __name__ == '__main__':
+    main()
--- problem_two_transcription/main.py
+++ problem_two_transcription/main.py
@@ -2,7 +2,7 @@ import os
 import sys 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from DNA_functions import *
+from Rosalind_functions import *
 from structures import *
 
 
@@ -12,7 +12,7 @@ def main():
         seq = file.read()
     seq = seq.upper()
 
-    print(transcribe(seq))
+    print(dna_rna_convert(seq))
     
 
 
